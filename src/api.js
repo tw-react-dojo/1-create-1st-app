33254942d0b44d3fcae4a57215094ef012f0d64a
Cristian Caroli <cristiancaroli@Cristians-MacBook-Pro.local>
2017-06-23 19:10:14 +0200

    https

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 export const getAstronauts = () =>
-  fetch('http://api.open-notify.org/astros.json').then(response =>
+  fetch('https://api.open-notify.org/astros.json').then(response =>
     response.json()
   )
